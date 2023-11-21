3b8a58f563241e179cafe14aa0406eca690d4b7e
maruf-py <marufur1847@gmail.com>
2023-11-22 03:36:17 +0600

    Naming Convention add

---
+++ Class Object/src/NamingConvention.java
@@ -0,0 +1,34 @@
+public class NamingConvention {
+    public static void main(String[] args) {
+        /*
+        In java, it is good practice to name class, variables, and methods name as what they are actually
+        supposed to do instead of naming them randomly. Below are some naming conventions of the java programming language
+
+
+        Type 1: Classes and Interfaces
+        class names should be nouns, in mixed cases with the first letter of each internal word capitalized.
+        Interfaces names should also be capitalized just like class names.
+        Use whole words and must avoid acronyms and abbreviations.
+
+        Type 2: Methods
+        Methods should be verbs, in mixed case with the first letter lowercase and with the first letter
+        of each internal word capitalized.
+
+        Type 3: Variables
+        Variable names should be short yet meaningful.
+        Variable names should not start with underscore _ or dollar sign $ characters, even though both are allowed.
+        Should be mnemonic i.e, designed to indicate to the casual observer the intent of its use.
+        One-character variable names should be avoided except for temporary variables.
+        Common names for temporary variables are i, j, k, m, and n for integers; c, d, and e for characters.
+
+        Type 4: Constant variables
+        Should be all uppercase with words separated by underscores (“_”).
+        There are various constants used in predefined classes like Float, Long, String etc
+
+        Type 5: Packages
+        The prefix of a unique package name is always written in all-lowercase
+                ASCII letters and should be one of the top-level domain names, like com, edu, gov, mil, net, org.
+
+        */
+    }
+}
