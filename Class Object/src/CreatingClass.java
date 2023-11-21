0d1e740070f1d860bfd55b66dd95804af6938849
maruf-py <marufur1847@gmail.com>
2023-11-22 00:06:55 +0600

    Initial Commit

---
+++ Class Object/src/CreatingClass.java
@@ -0,0 +1,10 @@
+//A Class is like an object constructor, or a "blueprint" for creating objects.
+
+public class CreatingClass { //To create a class, use the keyword class:
+    int a = 5; // crating class with variable
+
+    public static void main(String[] args) {
+        
+    }
+}
+
