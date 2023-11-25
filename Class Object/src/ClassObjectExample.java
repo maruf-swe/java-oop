7d78d5a087e0bccf33517a50c11cb2d3d6a6b40a
Marufur Rahman <59738629+maruf-swe@users.noreply.github.com>
2023-11-25 23:23:11 +0600

    Update ClassObjectExample.java

--- Class Object/src/ClassObjectExample.java
+++ Class Object/src/ClassObjectExample.java
@@ -18,10 +18,12 @@ public class ClassObjectExample {
         return name;
     }
 
+    
     // method 2
     public String getBreed() {
         return breed;
     }
+    
 
     // method 3
     public int getAge() {
@@ -32,11 +34,13 @@ public class ClassObjectExample {
     public String getColor() {
         return color;
     }
+    
 
     public String toString(){
         return ("Hi my name is "+ this.getName() +"\nMy Bread is "+this.getBreed() + " my color is "+this.getColor());
     }
 
+    
     public static void main(String[] args) {
         ClassObjectExample clob = new ClassObjectExample("Reddy","Germany", 5,"Black");
         System.out.println(clob.age);
